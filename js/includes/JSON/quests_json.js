ea13e501d8a3703887ea3560ce9ced15dcaa5633
Maia McGuinness <maiamcguinness@gmail.com>
2014-04-13 13:07:51 -0500

    Added organization image & description for NASA

--- js/includes/JSON/quests_json.js
+++ js/includes/JSON/quests_json.js
@@ -4,13 +4,41 @@
             "name": "Melting Polar Ice Cap",
             "image": "",
             "description": "Oldest and thickest Arctic sea ice is disappearing.",
-            "icons": "",
+            "icons": [
+                1,
+                2,
+                3
+            ],
             "stars": 4,
             "science types": [
                 "geosphere",
                 "biosphere",
                 "cosmosphere"
-            ]
+            ],
+            "age": "13+",
+            "organization": "NASA",
+            "organization_img": "images/nasa.png",
+            "organization_descrip": "NASA.gov brings you images, videos, and interactive features from the unique persepctive of America's space agency."
+        },
+        {
+            "name": "Coastal Inundation",
+            "image": "",
+            "description": "How does ice cap melting affect our regional areas?",
+            "icons": [
+                1,
+                2,
+                3
+            ],
+            "stars": 4,
+            "science types": [
+                "geosphere",
+                "biosphere",
+                "cosmosphere"
+            ],
+            "age": "14+",
+            "organization": "NASA",
+            "organization_img": "images/nasa.png",
+            "organization_descrip": "NASA.gov brings you images, videos, and interactive features from the unique persepctive of America's space agency."
         }
     ]
 }
